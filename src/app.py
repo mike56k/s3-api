4471a9c921db1db880c69e0a6965405f39638f50
Mikhail <isachenko.mikhail.k@gmail.com>
2023-12-30 20:07:34 +0300

    Add swagger

--- src/app.py
+++ src/app.py
@@ -1,23 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restx import Api, Resource, fields
 import os
 
 app = Flask(__name__)
 app.config["FILES_DIR"] = "/mnt/s3"
 
-@app.route('/files', methods=['GET'])
-def get_file_list():
-    files = os.listdir(app.config["FILES_DIR"])
-    return jsonify({"files": files}), 200
+api = Api(app)
 
-@app.route('/files/<filename>', methods=['GET'])
-def get_file_content(filename):
-    file_path = os.path.join(app.config["FILES_DIR"], filename)
-    if os.path.isfile(file_path):
-        with open(file_path, 'r') as file:
-            content = file.read()
-        return jsonify({"filename": filename, "content": content})
-    else:
-        return jsonify({"error": "File not found"}), 404
+class FileList(Resource):
+    @api.doc('GET')
+    @api.response(200, 'List of files')
+    def get(self):
+        files = os.listdir(app.config['FILES_DIR'])
+        return {'files': files}
+
+class FileContent(Resource):
+    @api.doc('GET')
+    @api.response(200, 'Content of the file')
+    def get(self, filename):
+        file_path = os.path.join(app.config['FILES_DIR'], filename)
+        if os.path.isfile(file_path):
+            with open(file_path, 'r') as file:
+                content = file.read()
+                return {'filename': filename, 'content': content}
+        else:
+            return {'error': 'File not found'}, 404
+
+api.add_resource(FileList, '/files')
+api.add_resource(FileContent, '/files/<filename>')
 
 if __name__ == '__main__':
     app.run()
